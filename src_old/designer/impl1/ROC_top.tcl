beb43a2219aa7b59327a17199a2d16f4d4c99991
curlywei <curlywei@gmail.com>
2021-04-08 15:34:38 +0800

    1st

---
+++ src_old/designer/impl1/ROC_top.tcl
@@ -0,0 +1,14 @@
+# Created by Libero Project Manager 9.1.5.1
+# Wed Oct 23 16:08:57 2013
+
+# (OPEN DESIGN)
+
+
+# set default back-annotation base-name
+set_defvar "BA_NAME" "ROC_top_ba"
+set_defvar "IDE_DESIGNERVIEW_NAME" {Impl1}
+set_defvar "IDE_DESIGNERVIEW_COUNT" "1"
+set_defvar "IDE_DESIGNERVIEW_REV0" {Impl1}
+set_defvar "IDE_DESIGNERVIEW_REVNUM0" "1"
+set_defvar "IDE_DESIGNERVIEW_ROOTDIR" {C:\FVTX_VHDL_code\ROC_main_FPGA_B_D\designer}
+set_defvar "IDE_DESIGNERVIEW_LASTREV" "1"
